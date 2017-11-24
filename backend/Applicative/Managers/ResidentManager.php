8c9fec77006185e450242c6f574a83c04be93367
Clovis Portron <chaipokoi@gmail.com>
2017-11-24 14:28:00 +0100

    Ajout des informations complémentaires sur les residents

--- backend/Applicative/Managers/ResidentManager.php
+++ backend/Applicative/Managers/ResidentManager.php
@@ -15,7 +15,46 @@ class ResidentManager implements IModelManager
 
     public static function Get($id)
     {
-        return ModelManager::Get("Resident", $id);
+        $resident =  ModelManager::Get("Resident", $id);
+        if($resident == null)
+            return null;
+        $resident = get_object_vars($resident);
+    
+        // Récupération des ats et dms
+        $tool_residents = Tool_ResidentManager::GetAll("Resident_id eq "+$id);
+        $ats = array();
+        $dms = array();
+        foreach($tool_residents as $link)
+        {
+            $tool = Tool::Get($link->getToolId());
+            if($tool == null)
+                continue;
+            if($tool->getType() == 0)
+                array_push($dms, $tool);
+            else if($tool->getType() == 1)
+                array_push($ats, $tool);
+        }
+        $resident["ats"] = $ats;
+        $resident["dms"] = $dms;
+
+        // Récupération s des users
+        $user_residents = User_ResidentManager::GetAll("Resident_id eq "+$id);
+        $perso = array();
+        $pro = array();
+        foreach($user_residents as $link)
+        {
+            $user = Tool::Get($link->getUserId());
+            if($user == null)
+                continue;
+            if($user->getGroup() == 0)
+                array_push($perso, $user);
+            else if($user->getGroup() == 1)
+                array_push($pro, $user);   
+        }
+        $resident["perso"] = $perso;
+        $resdient["pro"] = $pro;
+
+        return $resident;
     }
 
     public static function Put($lastname, $firstname, $age, $pathologies, $autonomy, $Establishment_id, $healplan = null, $picture = null)
