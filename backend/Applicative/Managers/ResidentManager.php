8019ac21004eeb19bac5ac112278c45064de135b
Clovis Portron <chaipokoi@gmail.com>
2017-12-05 19:22:14 +0100

    500

--- backend/Applicative/Managers/ResidentManager.php
+++ backend/Applicative/Managers/ResidentManager.php
@@ -26,7 +26,7 @@ class ResidentManager implements IModelManager
         $dms = array();
         foreach($tool_residents as $link)
         {
-            $tool = ToolManager::Get($link->getToolId());
+            $tool = ToolManager::Get($link["Tool_id"]);
             if($tool == null)
                 continue;
             if($tool->getType() == 0)
@@ -43,7 +43,7 @@ class ResidentManager implements IModelManager
         $pro = array();
         foreach($user_residents as $link)
         {
-            $user = ToolManager::Get($link->getUserId());
+            $user = ToolManager::Get($link["User_id"]);
             if($user == null)
                 continue;
             if($user->getGroup() == 0)
