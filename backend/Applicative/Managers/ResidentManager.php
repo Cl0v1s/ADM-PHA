0cfdbdd4404f610be2ca554606c5e82eaf40fdd0
Clovis Portron <chaipokoi@gmail.com>
2018-01-21 16:32:21 +0100

    dsq

--- backend/Applicative/Managers/ResidentManager.php
+++ backend/Applicative/Managers/ResidentManager.php
@@ -52,7 +52,7 @@ class ResidentManager implements IModelManager
         $pro = array();
         foreach($user_residents as $link)
         {
-            $user = ToolManager::Get($link->getUserId());
+            $user = UserManager::Get($link->getUserId());
             if($user == null)
                 continue;
             if($user->getGroup() == 0)
